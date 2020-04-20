c547149cc16825b19b7af46dd6b3cf9bc6ac519f
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-04-20 21:33:25 +0300

    cleaning

--- models/product.js
+++ models/product.js
@@ -2,12 +2,6 @@ var mongoose = require('mongoose');
 //  Объявляем схему,  как свойство mongoose
 var Schema = mongoose.Schema;
 
-// var productSchema = new Schema({
-//     imagePath: { type: String, required: true },
-//     title: { type: String, required: true },
-//     description: { type: String, required: true },
-//     price: { type: Number, required: true }
-// }, { collection: 'shopping' });
 var productSchema = new Schema({
     imagePath: { type: String, required: true },
     title: { type: String, required: true },
