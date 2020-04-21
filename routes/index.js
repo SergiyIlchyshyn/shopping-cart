c60415f1c21485fc0453a07b78cfe63bca59e964
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-04-21 18:17:43 +0300

    page add product

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Product.find(function(err, docs) {
+    Product.find(function(err, products) {
         // console.log(docs);
         // var products = [];
         // if (err) {
@@ -21,8 +21,36 @@ router.get('/', function(req, res, next) {
         //     // return res.json(docs);
         // }
 
-        res.render('shop/index', { title: 'Shopping Cart', products: docs });
+        res.render('shop/index', { title: 'Women Clothing Store', products: products });
     });
+});
+
+router.get('/add-product', function(req, res, next) {
+    res.render('shop/add-product', {
+        title: 'Create'
+    });
+});
+//отримати дані і зберегти їх
+router.post('/add-product', function(req, res, next) {
+    console.log(req.body);
+    //записати в БД
+    res.redirect('/');
+});
+//отримати дані і зберегти їх
+router.get('/edit-product/:id', function(req, res, next) {
+    res.render('shop/edit-product', {
+        title: 'Edit'
+    });
+});
+router.post('/edit-product/:id', function(req, res, next) {
+    console.log(req.body);
+    //записати в БД
+    res.redirect('/');
+});
+router.get('/delete-product/:id', function(req, res, next) {
+    console.log(req.params.id);
+    //видалити
+    res.render('shop/delete-product', { title: 'Delete', message: 'Deleted!' });
 });
 
 module.exports = router;
