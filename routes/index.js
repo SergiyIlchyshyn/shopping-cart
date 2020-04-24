cd532358a6e661c5e57785d1f1f551dbc3f110a2
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-04-24 18:59:08 +0300

    added cart model

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -88,4 +89,21 @@ router.get('/delete-product/:id', function(req, res, next) {
         })
 });
 //==============================================================================
+
+//ADD-TO-CART===================================================================
+router.get('/add-to-cart/:id', function(req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : { items: {} })
+
+    Product.findById(productId, function(err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
